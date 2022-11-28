215eab17d94d9fdde6de3a514930aa2dc4fc0c69
EC2 Default User <ec2-user@ip-172-31-92-119.ec2.internal>
2022-11-28 04:08:12 +0000

    Added the code to create subnets

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 }
+
+resource "aws_subnet" "main" {
+  count      = length(var.subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet-[count.index]"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable vpc_cidr_block {
   description = "The CIDR Block of the VPC"
   default = "10.0.0.0/16"
 }
+
+variable subnet_cidr {
+  type = list
+  description = "The cidr blocks pertaining to the subnets"
+  default = ["10.0.1.0/24", "10.0.3.0/24"]
+{
