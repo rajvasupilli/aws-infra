93a81f9b0e10a8de89137c39d0461ed0ce73ae1f
EC2 Default User <ec2-user@ip-172-31-92-119.ec2.internal>
2022-11-28 04:25:54 +0000

    Added resource dependency

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable vpc_cidr_block {
   type = string
   description = "The CIDR Block of the VPC"
-  default = "10.0.0.0/16"
+  default = "10.20.0.0/16"
 }
 
 variable subnet_cidr {
   type = list
   description = "The cidr blocks pertaining to the subnets"
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.20.1.0/24", "10.20.2.0/24"]
 }
