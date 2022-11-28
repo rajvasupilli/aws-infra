019ecd6e6a3a92ee7305e8d827b2fdaf8e0e8560
EC2 Default User <ec2-user@ip-172-31-92-119.ec2.internal>
2022-11-28 04:41:45 +0000

    Added resource dependency

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_subnet" "main" {
   cidr_block = element(var.subnet_cidr,count.index)
 
   tags = {
-    Name = "subnet-${count.index}"
+    Name = "subnet-${count.index+1}"
   }
  depends_on = ["aws_vpc.main"]
 }
